7b75256baaf63eec660fc078e6256c6a289651ee
snail-max <2991738411@qq.com>
2021-05-12 20:57:30 +0800

    检查元素是否为空

---
+++ python习题/python基础/函数编程/习题3_检查元素为空.py
@@ -0,0 +1,19 @@
+# -*-coding:utf-8-*-
+
+
+
+
+def cheackelement(*args):
+    len_num = len(args)
+    if len_num:
+        return args
+    else:
+        return None
+
+
+
+
+
+lis = [1,2,3]
+tuple = ()
+print(cheackelement(tuple))
