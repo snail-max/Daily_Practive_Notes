299067eac412d6d4bffb61c52962f0c54aed30f7
snail-max <2991738411@qq.com>
2021-05-15 10:03:23 +0800

    函数阶乘

--- python习题/python基础/函数编程/习题9_阶乘.py
+++ python习题/python基础/函数编程/习题9_阶乘.py
@@ -6,10 +6,8 @@
 def factorial(n):
     sum = 1
     for i in range(2,n+1):
-        print(i)
         sum = sum * (i-1)
     else:
-        print(sum)
         return sum
 
 if __name__ == '__main__':
